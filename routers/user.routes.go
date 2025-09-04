4d8b8c67cda52a35a000177bd8db5eae35b029e5
AnonymousXDragon <rohitAsAnonymous@proton.me>
2025-09-04 09:09:48 +0530

    rate limit added

--- routers/user.routes.go
+++ routers/user.routes.go
@@ -7,8 +7,10 @@ import (
 	middle "falcon/middlewares"
 	UserRepo "falcon/repository"
 	UserSrv "falcon/services"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func UserRoutesInit(app *fiber.App) {
@@ -18,7 +20,20 @@ func UserRoutesInit(app *fiber.App) {
 	cnt := controllers.UserNewController(userService)
 
 	app.Route("/", func(api fiber.Router) {
-		api.Post("/signup", middle.Validator(types.UserRegister{}), cnt.RegisterHandler)
+		api.Post("/signup",
+			limiter.New(limiter.Config{
+				Max:        25,
+				Expiration: 30 * time.Minute,
+				LimitReached: func(c *fiber.Ctx) error {
+					return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+						"status": "error",
+						"error":  "Too many requests, please try again later",
+					})
+				},
+			}),
+			middle.Validator(types.UserRegister{}),
+			cnt.RegisterHandler,
+		)
 		api.Post("/login", middle.Validator(types.UserLogin{}), cnt.LoginHandler)
 	})
 }
